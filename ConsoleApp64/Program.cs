b9086ed26aa0d7942d37757223e9e8055f9c11ed
sulton <global-sultonmalik326@gmail.com>
2024-01-19 17:48:40 +0500

    Add project files.

---
+++ ConsoleApp64/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+using TelegramBotExample;
+
+namespace ConsoleApp58;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        const string token = "6719531096:AAGHUFOcuu5g-JDh0FT1ecif6yLAYswyAXM";
+
+        TelegramBotClass telegramPost = new TelegramBotClass(token);
+        await telegramPost.BotHandle();
+    }
+}
---
+++ ConsoleApp64/TelegramBotClass.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace TelegramBotExample
+{
+    public class TelegramBotClass
+    {
+        private string Token;
+        private string channelName;
+        private string postText;
+        private string photoOrVideoUrl;
+        private DateTime postCreationTime;
+        private bool isEditingPost;
+
+        public TelegramBotClass(string token)
+        {
+            this.Token = token;
+        }
+
+        public async Task BotHandle()
+        {
+            var botClient = new TelegramBotClient(Token);
+            using CancellationTokenSource cts = new();
+
+            botClient.StartReceiving(
+                updateHandler: async (bot, update, ct) => await HandleUpdateAsync(bot, update, ct),
+                pollingErrorHandler: HandlePollingErrorAsync,
+                cancellationToken: cts.Token
+            );
+
+            var me = await botClient.GetMeAsync();
+            Console.WriteLine($"Start listening for @{me.Username}");
+            Console.ReadLine();
+
+            cts.Cancel();
+        }
+
+    
+
+        public async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            if (update.Message is not { } message || message.Text is not { } messageText)
+                return;
+
+            var chatId = message.Chat.Id;
+
+
+            string replacemessage = update.Message.Text.Replace("www.", "dd");
+
+            if (update.Message.Text.StartsWith("https://www.instagram.com"))
+            {
+                Message sentMessage = await botClient.SendPhotoAsync(
+                    chatId: chatId,
+                    photo: replacemessage,
+                  //  supportsStreaming: true,
+                    cancellationToken: cancellationToken
+                );
+            }
+
+   
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+        private Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+            var ErrorMessage = exception switch
+            {
+                ApiRequestException apiRequestException
+                    => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+                _ => exception.ToString()
+            };
+
+            Console.WriteLine(ErrorMessage);
+            return Task.CompletedTask;
+        }
+    }
+}
